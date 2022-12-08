7ac7ef61a98b978a376892b68f8c2d75ecb288e8
Alonso Anselmo Gómez Sánchez <68868495+AAGS589@users.noreply.github.com>
2022-12-08 14:05:11 -0600

    purebas

--- src/components/modules/particiones/body.jsx
+++ src/components/modules/particiones/body.jsx
@@ -1,30 +1,39 @@
 import '../../../assets/stylesheets/body.css'
+
 import { useQuery } from '@tanstack/react-query';
 
 import {useState, useEffect} from 'react';
 import { getParticiones } from '../../../api/Api';
 
+
 function Cuerpo(){
-    
 
+  
 
+  const [conju,setConjun] = useState([]);
+
+  
     const {isLoading, data, isError, error} = useQuery({
-      queryKey: ['conjunto'],
+      queryKey: ['particion'],
+      
       queryFn: getParticiones
     })
 
     if (isLoading) return <div>Loading...</div>
     else if (isError) return <div>Error: {error.message}</div>
 
+  
+
 
     const preguntas = [
         {
             titulo: "Dado el conjunto: ",
             opciones: [
-                {textoRespuesta: "ingresar sd", isCorrect: true},
-                {textoRespuesta: "ingresar opcion", isCorrect: false},
-                {textoRespuesta: "ingresar opcion", isCorrect: true},
-                {textoRespuesta: "ingresar opcion", isCorrect: false},
+                
+              {textoRespuesta: "", isCorrect: false},
+                {textoRespuesta: "", isCorrect: false},
+                {textoRespuesta: "", isCorrect: true},
+                {textoRespuesta: "", isCorrect: false},
             ],
         },
     
@@ -40,56 +49,33 @@ function Cuerpo(){
         <div className="lado-izquierdo">
                 <div className="pregunta-numero">
                     <button className='exam'>Generar conjunto aleatorio</button>
-                    <div className="titulo-pregunta">{preguntas[preguntaActual].titulo},{data}</div>
+                    <div className="titulo-pregunta">{preguntas[preguntaActual].titulo}</div>
                 </div>
             </div>
 
             <div className="lado-derecho">
                 {preguntas[preguntaActual].opciones.map((respuesta) =>(
-                    <button className='quiz'>{respuesta.textoRespuesta}</button>
+                    <button className='quiz'></button>
                 ))  }
             </div>
         </div>
-         {/* seccion del formulario */}
+         
 
-         <form action="">
+         
+        <form >
          <div className="container">
         <div className="lado-izquierdo">
                 <div className="pregunta-numero">
-                    <span>Pregunta {preguntaActual + 1} de</span> {preguntas.length}
+                    <span>Pregunta 1</span> 
                     <div className="titulo-pregunta">Ingrese un conjunto a resolver</div>
                 </div>
             </div>
-
             <div className="lado-derecho">
-                {/* input que mandara el conjunto */}
                 <input className='input' type="text" id='conjunto'/>
-                <button type="button" className='quiz' data-bs-toggle="modal" data-bs-target="#exampleModal">Enviar respuesta</button>
-                
+                <button type="button" className='quiz'>Enviar respuesta</button>
             </div>
-
-            {/* Modal */}
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Resultados</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        
-      </div>
-    </div>
-  </div>
-</div>
-
         </div>
          </form>
-        
 
         
         <div className="container">
